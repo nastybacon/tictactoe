de220e95f0efeb7fcde2bdf5f7294c3bdb36d3b1
nastybacon <126672145+nastybacon@users.noreply.github.com>
2023-03-05 12:14:57 +0100

    show filled shapes

--- script.js
+++ script.js
@@ -11,4 +11,19 @@ function fillShape(id) { // Funktion für abwechselnde Cross oder Circle
 
     fields[id] = currentShape; // an dieser Stelle entweder Circle oder Cross 
     console.log(fields); // zur Kontrolle - funktioniert es wirklich?
+    draw(); // Spielfeld neu malen
+}
+
+function draw(){ // Spielfeld neu malen | wird nach jedem td Element neu aufrufen
+    for (let i = 0; i < fields.length; i++) { // für alle 9 Felder wiederholen 
+        if(fields[i] == 'circle') { // wenn Element ein Kreis ist, dann
+            document.getElementById('circle-' + i).classList.remove('d-none'); 
+            // entferne die d-none Klasse beim Kreis = zeige Kreis an
+         }
+        if(fields[i] == 'cross') { // wenn Element ein Kreuz ist, dann
+            document.getElementById('cross-' + i).classList.remove('d-none'); 
+            // entferne die d-none Klasse beim Kreuz = zeige Kreuz an
+        }
+        
+    }
 }
