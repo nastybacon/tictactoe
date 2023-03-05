b4c3446f0bab5fc6cb8f6b4271d108984fec8d28
nastybacon <126672145+nastybacon@users.noreply.github.com>
2023-03-05 11:35:43 +0100

    array + fillShape(id)

---
+++ script.js
@@ -0,0 +1,6 @@
+let fields = []; // Array definieren
+
+function fillShape(id) { // für jeden Kasten eine ID geben + 
+    fields[id] = 'cross'; // an dieser Stelle entweder Circle oder Cross 
+    console.log(fields); // zur Kontrolle - funktioniert es wirklich?
+}
