cea91934620e939e00ff7fdd7514d667f7fa3899
nastybacon <126672145+nastybacon@users.noreply.github.com>
2023-03-05 11:47:51 +0100

    change player

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 let fields = []; // Array definieren
 
-function fillShape(id) { // für jeden Kasten eine ID geben + 
-    fields[id] = 'cross'; // an dieser Stelle entweder Circle oder Cross 
+let currentShape = 'cross'; // erste Variable als Kreuz definieren
+
+function fillShape(id) { // Funktion für abwechselnde Cross oder Circle 
+    if (currentShape == 'cross') { // wenn aktuelle Kreuz ist, dann
+        currentShape = 'circle'; // wechseln wir zum Kreis
+    } else { // wenn nicht, dann
+        currentShape = 'cross'; // wechseln wir zum Kreuz
+    }
+
+    fields[id] = currentShape; // an dieser Stelle entweder Circle oder Cross 
     console.log(fields); // zur Kontrolle - funktioniert es wirklich?
 }
