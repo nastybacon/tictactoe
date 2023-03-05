e6e6d17c028e2d1fc265a63907c20c6b483d3797
nastybacon <126672145+nastybacon@users.noreply.github.com>
2023-03-05 16:45:08 +0100

    line animation

--- script.js
+++ script.js
@@ -40,29 +40,37 @@ function checkForWin() { // hat jemand gewonnen?
     // horizontal
     if (fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) { // wenn Feld 0 = Feld 1 = Feld 2 & auswerten, dann 
         winner = fields[0]; // Gewinner anzeigen 
+        document.getElementById('line-1').style.transform = 'scaleX(1)'; // die Linie markiert die Reihe des Gewinners
     }
     if (fields[3] == fields[4] && fields[4] == fields[5] && fields[4]) { //
         winner = fields[3];
+        document.getElementById('line-2').style.transform = 'scaleX(1)'; 
     }
     if (fields[6] == fields[7] && fields[7] == fields[8] && fields[6]) { //
         winner = fields[6];
+        document.getElementById('line-3').style.transform = 'scaleX(1)'; 
     }
     // vertikal
     if (fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) { //
         winner = fields[0];
+        document.getElementById('line-4').style.transform = 'rotate(90deg) scaleX(1)'; 
     }
     if (fields[1] == fields[4] && fields[4] == fields[7] && fields[1]) { //
         winner = fields[1];
+        document.getElementById('line-5').style.transform = 'rotate(90deg) scaleX(1)'; 
     }
     if (fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) { //
         winner = fields[2];
+        document.getElementById('line-6').style.transform = 'rotate(90deg) scaleX(1)'; 
     }
     // diagonal
     if (fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) { //
         winner = fields[0];
+        document.getElementById('line-7').style.transform = 'rotate(45deg) scaleX(1.5)'; 
     }
     if (fields[2] == fields[4] && fields[4] == fields[6] && fields[2]) { //
         winner = fields[2];
+        document.getElementById('line-8').style.transform = 'rotate(-45deg) scaleX(1.5)'; 
     }
     if (winner) { // wenn gewonnen, dann
         console.log('GEWONNEN:', winner); // Test | GEWONNEN anzeigen
